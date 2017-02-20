00d739d25538bd7ea6a108af42bdf3aa5e66eeb7
kicknpick <travisbpickering@gmail.com>
2017-02-19 20:30:00 -0600

    need help

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,22 +6,22 @@ var mysql = require("mysql");
 
 module.exports = function(app) {
   
-// MySQL DB Connection Information (remember to change this with our specific credentials)
-var connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "password",
-  database: "Bamazon_DB"
-});
+  // MySQL DB Connection Information (remember to change this with our specific credentials)
+  var connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "password",
+    database: "Bamazon_DB"
+  });
 
-// Initiate MySQL Connection.
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+  // Initiate MySQL Connection.
+  connection.connect(function(err) {
+    if (err) {
+      console.error("error connecting: " + err.stack);
+      return;
+    }
+    console.log("connected as id " + connection.threadId);
+  });
 
 
   // API GET Requests
@@ -57,21 +57,22 @@ connection.connect(function(err) {
 
 
   // Routes
-  app.get("/", function(req, res) {
+  app.get("/allproducts", function(req, res) {
     connection.query("SELECT * FROM products order by item_id", function(err, result) {
-      var html = "<h1> Product List By Department </h1>";
+      result.send();
+      // var html = "<h1> Product List By Department </h1>";
 
-      html += "<ul>";
+      // html += "<ul>";
 
-      for (var i = 0; i < result.length; i++) {
-        html += "<li><p> Product: " + result[i].product_name + "</p>"
-        html += "<p> Price: $ " + result[i].price + " </p>";
-        html += "<p> ID: " + result[i].item_id + "</p></li>";
-      }
+      // for (var i = 0; i < result.length; i++) {
+      //   html += "<li><p> Product: " + result[i].product_name + "</p>"
+      //   html += "<p> Price: $ " + result[i].price + " </p>";
+      //   html += "<p> ID: " + result[i].item_id + "</p></li>";
+      // }
 
-      html += "</ul>";
+      // html += "</ul>";
 
-      res.send(html);
+      // res.send(html);
     });
   });
 };
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -4,7 +4,7 @@ var path = require("path");
 module.exports = function(app) {
   // HTML GET Requests
 
-  app.get("/app", function(req, res) {
+  app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "/../public/index.html"));
   });
 
